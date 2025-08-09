78d8938f5ff8c76ed511ba6f1978d79c92298a0d
Pascal Mengelt <pascal.mengelt@gmail.com>
2025-08-09 21:06:59 +0200

    initial demo app.

---
+++ .scala-build/pme123-windspotter_de365c89c1-97c53e9127/src_generated/main/helper.scala
@@ -0,0 +1,21 @@
+
+
+//> using dep "com.lihaoyi::os-lib:0.11.3"
+
+
+@main
+def main(args: String*) =
+  val proc = "full"
+  println(s"Running ${proc}LinkJS")
+  os.proc("sbt", s"${proc}LinkJS").call()
+  os.proc("npm", "run", "build").call()
+  println("Adjusting file")
+  val indexPath = os.pwd / "dist" / "index.html"
+  val index = os.read(indexPath)
+  os.write.over(indexPath, index.replace("\"/assets/", "\"assets/"))
+  os.remove.all(os.pwd / "docs")
+  os.copy(os.pwd / "dist", os.pwd / "docs")
+  println("Copying the assets")
+  os.copy.over(os.pwd / "public", os.pwd / "docs" / "public")
+  //os.copy.over(os.pwd / "target" / "scala-3.6.2" / s"pme123-weather-${proc.replace("full", "")}opt.js", os.pwd / "pme123-weather.js")
+  println("Done")
